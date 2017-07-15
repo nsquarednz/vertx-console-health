a9daeb55bd1e8f49b8746c95f572646db4bc31ef
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-15 16:11:10 -0600

    Add resize directive (todo make global)

--- frontend/src/Health.vue
+++ frontend/src/Health.vue
@@ -1,22 +1,53 @@
 <style lang="scss" scoped>
+.health-container {
+    width: 100%;
+    height: 100%;
 
+    display: -webkit-box;
+
+    display: -ms-flexbox;
+
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+}
+
+.tree-display {
+    -webkit-box-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
+    margin-bottom: 20px;
+}
 </style>
 
 <template>
-    <div class="container-fluid status-container">
-        <pre v-html="JSON.stringify(healthChecks, null, 4)"></pre>
+    <div class="container-fluid health-container">
+        <div class="page-header">
+            <h1>Health Checks</h1>
+        </div>
+        <tree-display class="tree-display" :treeData="healthChecks"></tree-display>
     </div>
 </template>
 
 <script>
+import TreeDisplay from './TreeDisplay.vue';
+
 export default {
-    name: 'Health',
+    name: 'Health Checks',
+    components: {
+        'tree-display': TreeDisplay
+    },
     data() {
         return {
             healthChecks: {},
         }
     },
     beforeMount() {
+        /* Chrome will log errors no matter what: 
+           https://stackoverflow.com/questions/21990036/prevent-google-chrome-log-xmlhttprequest
+        */
         const updateStatuses = () => this.$http.get(window.location.pathname + '/healthchecks')
             .then(response => response.json(), errResponse => {
                 if (errResponse.status === 500 || errResponse.status === 503) {
@@ -27,18 +58,17 @@ export default {
             })
             .then(healthJson => {
                 healthJson.id = 'root';
-                healthJson.parent = null;
 
-                function setParentField(parentName, checksArr) {
-                    for (let checkObj of checksArr) {
-                        checkObj.parent = parentName;
-                        if (checkObj.checks) {
-                            setParentField(checkObj.id, checkObj.checks);
+                function setParentField(parentName, node) {
+                    node.parent = parentName;
+                    if (node.checks) {
+                        for (let checkObj of node.checks) {
+                            setParentField(node.id, checkObj);
                         }
                     }
                 }
 
-                setParentField(healthJson.id, healthJson.checks);
+                setParentField(null, healthJson);
                 this.healthChecks = healthJson;
             });
         updateStatuses();
---
+++ frontend/src/TreeDisplay.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-resize:throttle="onResize">
+        {{ treeData }}
+    </div>
+</template>
+
+
+<script>
+import resize from 'vue-resize-directive';
+
+export default {
+    props: {
+        treeData: Object
+    },
+    directives: {
+        resize
+    },
+    methods: {
+        onResize() {
+            console.log("resized")
+        }
+    }
+}
+</script>
